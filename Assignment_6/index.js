546d4f62c6cc74c846f7d2102f89ca1574ce0052
adityamulik <aditya.mulik@gmail.com>
2022-03-16 09:32:39 +0530

    edit, delete and getAll API endpoints completed

--- Assignment_6/index.js
+++ Assignment_6/index.js
@@ -1,4 +1,3 @@
-
 const express = require("express"),
       app = express(),
       mongoose = require("mongoose"),
@@ -6,6 +5,8 @@ const express = require("express"),
       bodyParser = require("body-parser");
 
 const saltRounds = 10;
+
+let counter = 1;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,40 +17,103 @@ mongoose.connect("mongodb://localhost/assignment6", {
 });
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  email: String,
   password: String,
+  user_type: String,
   joined: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("user", userSchema);
 
+// Home Page
 app.get("/", (req, res) => {
   res.send("Welcome to Assignment 6 - INFO6150.");
 });
 
 // Create new user
 app.post("/user/create", async (req, res) => {
-  console.log(req.body);
+
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
     const innerResult = await User.create({
-      username: req.body.email,
-      password: hashedPassword
+      _id: counter,
+      email: req.body.email,
+      password: hashedPassword,
+      user_type: req.body.user_type
     });
     res.status(201).send(innerResult);
+    counter += 1;
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error Occurred!");
   }
 });
 
-// Get all users
-app.get("/user/getAll", async (req, res) => {
-  const result = User.find({}, (err, users) => {
-    console.log(users);
-  })
+// Update user details
+
+app.post("/user/edit", async (req, res) => {
+
+  const id = req.query.id;
+
+  User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update User with id=${id}. User was not found!`
+        });
+      } else res.send({ message: "User details were updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating User with id=" + id
+      });
+    });
 });
 
+// Get all users
+app.get("/user/getAll", async (req, res) => {
+
+  User.find({}, function (err, users) {
+      users.forEach(user => delete user.password);
+      const newResult = users.map(item => {
+        return {
+          id: item._id,
+          email: item.email,
+          user_type: item.user_type
+        }
+      })
+      res.send(newResult);
+  });
+  
+});
+
+// Delete user
+
+app.delete("/user/delete", async (req, res) => {
+
+  const id = req.query.id;
+
+  User.findByIdAndDelete(id)
+    .then(item => {
+      if (!item) {
+        res.status(404).send({
+          message: `Cannot delete User with id=${id}. User not found!`
+        });
+      } else {
+        res.send({
+          message: `User with id ${id} was deleted successfully!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete User with id=" + id
+      });
+    });
+  
+});
+
+// Server config block
 app.listen(8000, () => {
   console.log("Server started at port 8000");
 });
