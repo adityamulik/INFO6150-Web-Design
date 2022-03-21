1cdf8095dd4fadd0e8c7d34902e9eb38398010dd
adityamulik <aditya.mulik@gmail.com>
2022-03-21 05:42:25 +0530

    added validations and updated APIS

--- Assignment_6/index.js
+++ Assignment_6/index.js
@@ -1,3 +1,5 @@
+const { validateEmail, checkPassword } = require("./validations");
+
 const express = require("express"),
       app = express(),
       mongoose = require("mongoose"),
@@ -5,8 +7,6 @@ const express = require("express"),
       bodyParser = require("body-parser");
 
 const saltRounds = 10;
-
-let counter = 1;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,7 +19,6 @@ mongoose.connect("mongodb://localhost/assignment6", {
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  user_type: String,
   joined: { type: Date, default: Date.now },
 });
 
@@ -34,40 +33,113 @@ app.get("/", (req, res) => {
 app.post("/user/create", async (req, res) => {
 
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-    const innerResult = await User.create({
-      _id: counter,
-      email: req.body.email,
-      password: hashedPassword,
-      user_type: req.body.user_type
-    });
-    res.status(201).send(innerResult);
-    counter += 1;
+
+    let user = await User.findOne({ email: req.body.email });
+    let passBool, emailBool = false;
+
+    if (user) {
+      res.status(400).send({ message: "Email Address already exists." });
+    } else {
+
+      if (validateEmail(req.body.email)) {
+        // console.log("Proper email address");
+        emailBool = true;
+      } else {
+        emailBool = false;
+        res.status(400).send({ message: "Please input email address correctly!"});
+      }
+
+      if (checkPassword(req.body.password) && (req.body.password == req.body.confirm_password)) {
+        passBool = true;
+        // console.log("Password is correct");
+      } else {
+        passBool = false;
+        res.status(400).send({ message: "Please input password correctly!"});
+      }
+
+      if (passBool && emailBool) {
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        const innerResult = await User.create({
+          email: req.body.email,
+          password: hashedPassword,
+          user_type: req.body.user_type
+        });
+        res.status(201).send(innerResult);
+      }   
+    }    
   } catch (error) {
     console.log(error);
-    res.status(500).send("Internal Server Error Occurred!");
+    res.status(500).send({ message: "Internal Server Error Occurred!"});
   }
+
 });
 
 // Update user details
-
 app.post("/user/edit", async (req, res) => {
 
-  const id = req.query.id;
+  const user = await User.findOne({email: req.body.email});
+  
+  if (user) {
+    const passCompare = await bcrypt.compare(req.body.password, user.password);
+    if (passCompare) {
 
-  User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then(data => {
-      if (!data) {
-        res.status(404).send({
-          message: `Cannot update User with id=${id}. User was not found!`
-        });
-      } else res.send({ message: "User details were updated successfully." });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating User with id=" + id
+      if (req.body.new_email != undefined && req.body.new_password != undefined && req.body.confirm_new_password != undefined) {
+        res.status(400).send({ message: "Please provide either new email or new password parameters only1" });
+      } else if (req.body.new_email != undefined && req.body.new_password == undefined && req.body.confirm_new_password == undefined) {
+        // console.log("Update new email");
+        if (validateEmail(req.body.new_email)) {
+          // console.log("Seems good");
+          User.findByIdAndUpdate(user._id, { email: req.body.new_email }, { useFindAndModify: false })
+            .then(data => {
+              if (!data) {
+                res.status(404).send({
+                  message: `Cannot update email with user id=${user._id}. User was not found!`
+                });
+              } else {
+                res.send({ message: "User email address was updated successfully." })
+              };
+            })
+            .catch(err => {
+              res.status(500).send({
+                message: "Error updating User's email with id=" + user._id
+              });
+            });
+        } else {
+          res.status(400).send({ message: "Please enter the new email correctly!" });
+        }
+      } else if (req.body.new_email == undefined && req.body.new_password != undefined && req.body.confirm_new_password != undefined) {
+        // console.log("Update new password");
+        if (checkPassword(req.body.new_password) && req.body.new_password == req.body.confirm_new_password) {
+          const newPassword = await bcrypt.hash(req.body.new_password, saltRounds);
+          User.findByIdAndUpdate(user._id, { password: newPassword }, { useFindAndModify: false })
+            .then(data => {
+              if (!data) {
+                res.status(404).send({
+                  message: `Cannot update password with user id=${user._id}. User was not found!`
+                });
+              } else res.send({ message: "User password was updated successfully." });
+            })
+            .catch(err => {
+              res.status(500).send({
+                message: "Error updating User's password with id=" + user._id
+              });
+            });
+        } else {
+          res.status(400).send({ message: "Please enter password correctly!" });
+        }
+      } else {
+        res.status(400).send({ message: "Please provide either the new email or new password!" });
+      }
+    } else {
+      res.status(404).send({
+        message: `Password incorrect entered, please retry.`
       });
+    }
+  } else {
+    res.status(404).send({
+      message: `User was not found! Please check the email address.`
     });
+  }
 });
 
 // Get all users
@@ -79,7 +151,7 @@ app.get("/user/getAll", async (req, res) => {
         return {
           id: item._id,
           email: item.email,
-          user_type: item.user_type
+          password: item.password
         }
       })
       res.send(newResult);
@@ -88,7 +160,6 @@ app.get("/user/getAll", async (req, res) => {
 });
 
 // Delete user
-
 app.delete("/user/delete", async (req, res) => {
 
   const id = req.query.id;
